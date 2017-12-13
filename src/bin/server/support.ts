3a128bce3ed66d049d2b1b053038c12d4d2a62f9
Darin Morrison <freebroccolo@users.noreply.github.com>
2017-12-12 17:17:14 -0700

    WIP: renaming

--- src/bin/server/support.ts
+++ src/bin/server/support.ts
@@ -2,7 +2,7 @@ import * as LSP from "vscode-languageserver-protocol";
 
 export type AsyncRequestHandler<P, R> = (event: P, token: LSP.CancellationToken) => Thenable<R>;
 
-export function cancellableHandler<P, R>(worker: AsyncRequestHandler<P, R>): LSP.RequestHandler<P, R, void> {
+export function cancellableHandler<P, R>(handler: AsyncRequestHandler<P, R>): LSP.RequestHandler<P, R, void> {
   return (event, token) =>
-    Promise.race<R>([new Promise((_resolve, reject) => token.onCancellationRequested(reject)), worker(event, token)]);
+    Promise.race<R>([new Promise((_resolve, reject) => token.onCancellationRequested(reject)), handler(event, token)]);
 }
